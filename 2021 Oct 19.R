58a29b05ae19a2b802e2207517f726598afab3e0
Anthony Cruz <54594266+ac834815@users.noreply.github.com>
2021-10-19 23:03:54 -0400

    Add files via upload

--- 2021 Oct 19.R
+++ 2021 Oct 19.R
@@ -28,7 +28,7 @@ firstPlaceGiantsAgg$place <- as.character(firstPlaceGiantsAgg$place)
 giantAnim <- firstPlaceGiantsAgg %>% 
   ggplot(aes(x = year, y = weight, color = country, label = place)) +
   geom_line(size = 1.2) + geom_text_repel() + transition_reveal(year) +
-  scale_x_continuous(breaks = 2013:2021, labels = 2013:2021) + scale_color_manual(values = c("#AAAAAA", "#000000", "#666666")) +
+  scale_x_continuous(breaks = 2013:2021, labels = 2013:2021) + scale_color_manual(values = c("#FFCF00", "#FF8C00", "#FF4800")) +
   theme_minimal() + theme(legend.position = "bottom", legend.title = element_blank()) +
   labs(title = "Average Giant Pumpkin Weights in 2021's Top-Placing Countries", x = "Year", y = "Weight")
 animate(giantAnim, end_pause = 20)
